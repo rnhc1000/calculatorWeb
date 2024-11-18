68acc3aa0d62c2ef66c1fec38961f0abc7f8018d
Ricardo Ferreira <rnhc1000@hotmail>
2024-11-17 22:28:12 -0300

    [feat] ✨: add update_at timestamp at user entity every time the balance is changed.

--- src/main/java/br/dev/ferreiras/calculatorweb/dto/UserDto.java
+++ src/main/java/br/dev/ferreiras/calculatorweb/dto/UserDto.java
@@ -6,31 +6,32 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
 @Getter
 @Setter
 public class UserDto {
+
   private UUID id;
   private final String username;
   private final String status;
   private final Set<String> roles = new HashSet<>();
 
   public UserDto(final UUID id, final String username, final String status) {
+
     this.id = id;
     this.username = username;
     this.status = status;
 
   }
 
-  public UserDto(final Optional<User> entity) {
+  public UserDto(final User entity) {
 
-    this.id = entity.orElseThrow().getUserId();
-    this.username = entity.orElseThrow().getUsername();
-    this.status = entity.orElseThrow().getStatus();
-    for (final GrantedAuthority role : entity.orElseThrow().getAuthorities()) {
+    this.id = entity.getUserId();
+    this.username = entity.getUsername();
+    this.status = entity.getStatus();
+    for (final GrantedAuthority role : entity.getAuthorities()) {
       this.roles.add(role.getAuthority());
     }
 
